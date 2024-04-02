c4ee7a664011dbf51bae0ad589de436f8d1994c7
brianbreder <123673246+brianbreder@users.noreply.github.com>
2024-04-02 17:27:32 -0300

    .

---
+++ prog-02/ex-05/index.js
@@ -0,0 +1,8 @@
+var vel = 55
+console.log(`A velocidade do seu carro é ${vel}Km/h`)
+if (vel > 50) {
+    console.log(`Você ultrapassou o limite de velocidade. multado`)
+}else{
+    console.log('No limite de velocidade, perdemos seu dinheiro.') 
+}
+console.log('Dirija com segurança')
